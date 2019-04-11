13138095cde3436a591610a49ca34c03b1c118f1
XIAJIEXUWEI <18782247205@163.com>
2019-04-11 23:31:42 +0800

    新增搜索页

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,6 +13,19 @@ Page({
             content: e.detail.content,
             help: e.detail.help
         })
+    },
+  onShareAppMessage(msg){
+    if(msg.from==="menu"){
+      return {
+        title:"震惊！居然有如此简洁的小程序",
+        path:""
+      }
     }
-
+    else if (msg.from === "menu") {
+      return {
+        title: "文章标题",
+        path: ""
+      }
+    }
+  }
 })
