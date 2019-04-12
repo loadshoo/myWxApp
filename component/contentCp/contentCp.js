f5b3ec73b91c4e28701186cd1eb644fde5b6f3f9
J <18782247205@163.com>
2019-04-12 21:01:52 +0800

    修改内容组件,完成数据渲染以及页面跳转

---
+++ component/contentCp/contentCp.js
@@ -0,0 +1,43 @@
+// component/contentCp/content.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        datas: Array
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        time: "",
+        talk: "",
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        onShareAppMessage(msg) {
+            if (msg.from === "menu") {
+                return {
+                    title: "震惊！居然有如此简洁的小程序",
+                    path: ""
+                }
+            } else if (msg.from === "button") {
+                return {
+                    title: "文章标题",
+                    path: ""
+                }
+            }
+        },
+        getContent(msg) {
+            var index = msg.currentTarget.dataset['index'];
+            console.log(index)
+            wx.navigateTo({
+                    url: '/pages/content/content?id=' + index,
+                })
+        }
+    }
+})
