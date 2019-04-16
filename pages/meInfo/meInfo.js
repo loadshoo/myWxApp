f56cb84bdf16d6b18daecb8950a865e0da1f564d
J <18782247205@163.com>
2019-04-16 17:20:47 +0800

    优化用户登录后数据处理

--- pages/meInfo/meInfo.js
+++ pages/meInfo/meInfo.js
@@ -7,61 +7,87 @@ Page({
     data: {
         login: "",
         username: "",
-        userinfo: "",
+        userInfo: "",
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
-        let that = this;
-        wx.checkSession({
-            success(msg) {
+        var that = this;
+        wx.getStorage({
+            key: 'isLogin',
+            success: function(res) {
                 that.setData({
-                    login: true
-                });
-                wx.login({
-                    timeout: 10000,
-                    success(msg) {
-                        console.log(msg.code)
-                        if (msg.code) {
-                            wx.getUserInfo({
-                                success(res) {
-                                    that.setData({
-                                        userinfo: res.userInfo
-                                    });
-                                }
-                            })
-                        }
-                    }
+                    userInfo:res.data,
+                    login: true,
                 })
             },
-            fail(msg) {
-                that.setData({
-                    login: false
-                })
+            fail(res){
+                console.log(res)
             }
         })
     },
     //   未登录时点击登录
-    ftLogin() {
-        var that = this;
-        wx.login({
-            timeout: 10000,
-            success(msg) {
-                console.log(msg.code)
-                if (msg.code) {
-                    wx.getUserInfo({
-                        success(res) {
-                            that.setData({
-                                login: true,
-                                userinfo: res.userInfo
-                            });
-                        }
-                    })
+    ftLogin(e) {
+        var that = this
+        if (e.detail.errMsg === "getUserInfo:ok") {
+            //初始化云端数据库
+            wx.cloud.init;
+            //调用login云函数
+            wx.cloud.callFunction({
+                name: "login",
+                success(res) {
+                    //获取用户唯一标识openID
+                    let uuid = res.result.openid;
+                    const db = wx.cloud.database('start-project-8582df');
+                    //查询数据库中是否已保存用户数据
+                    db.collection('userInfo')
+                        .where({
+                            _openid: uuid
+                        }).get({
+                            success(msg) {
+                                //判断是否已保存过用户数据
+                                //大于0表示已存在，则不需要保存
+                                if (msg.data.length > 0) {
+                                    return
+                                } else {
+                                    //否则就添加用户数据到数据库
+                                    db.collection('userInfo').add({
+                                        data: {
+                                            avatarUrl: e.detail.userInfo.avatarUrl,
+                                            nickName: e.detail.userInfo.nickName
+                                        },
+                                        success(date) {
+
+                                        }
+                                    })
+                                }
+                            }
+                        })
+
                 }
-            }
-        })
+            })
+            wx.showToast({
+                title: '登录成功',
+            });
+            //设置缓存，判断是否登录
+            wx.setStorage({
+                key: 'isLogin',
+                data: e.detail.userInfo,
+            })
+            //无论数据库是否存在用户信息，都可以获取到信息
+            this.setData({
+                userInfo: e.detail.userInfo,
+                login: true,
+            })
+        } else {
+            wx.showToast({
+                title: '请允许授权',
+                icon: "none"
+            });
+        }
+
     },
     //退出登录
     logout() {
@@ -69,6 +95,13 @@ Page({
         that.setData({
             login: false,
             userInfo: ""
+        });
+        //清除缓存
+        wx.removeStorage({
+            key: 'isLogin',
+            success: function(res) {
+
+            },
         })
 
     },
