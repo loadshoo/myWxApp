77f42af151d40cb48f1e97b73623b1d5ce2ec395
J <18782247205@163.com>
2019-04-16 17:18:49 +0800

    新增用户反馈功能

--- component/helpCp/help.js
+++ component/helpCp/help.js
@@ -4,20 +4,68 @@ Component({
      * 组件的属性列表
      */
     properties: {
-
+        feedBackTxt: Object
     },
 
     /**
      * 组件的初始数据
      */
     data: {
-        
+        feedBackTxt: "",
+        txt: ""
     },
 
     /**
      * 组件的方法列表
      */
     methods: {
+        feedBack(txt) {
+            this.setData({
+                txt: txt.detail.value
+            })
+        },
+        feedSend() {
+            var that = this;
+            wx.getStorage({
+                key: 'isLogin',
+                success: function(res) {
+                    wx.cloud.init;
+                    const db = wx.cloud.database()
+                    db.collection('feedBack')
+                        .add({
+                            data: {
+                                feedTxt: that.data.txt,
+                                avatarUrl: res.data.avatarUrl,
+                                nickName: res.data.nickName,
+                            },
+                            success(msg) {
+                                db.collection('feedBack')
+                                    .get({
+                                        success(e) {
+                                            console.log(e)
+                                            that.setData({
+                                                feedBackTxt: e
+                                            })
+                                        }
+                                    })
+                                wx.showToast({
+                                    title: '发送成功',
+                                });
+                                that.setData({
+                                    txt: ""
+                                });
+                                
+                            }
+                        })
+                },
+                fail(res) {
+                    wx.showToast({
+                        title: '请先登录',
+                        icon: "none"
+                    })
+                }
+            })
+        },
+    },
 
-    }
 })
