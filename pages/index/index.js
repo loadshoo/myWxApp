8073397b9fa95aac9ebef2908c1f0385607114f6
J <18782247205@163.com>
2019-04-16 17:20:16 +0800

    优化从云数据库获取数据的方法

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,7 +8,8 @@ Page({
         help: false,
         canIUse: wx.canIUse('button.open-type.getUserInfo'),
         datas: "",
-        loading: false
+        loading: false,
+        feedBackTxt: ""
     },
     onMyEvent(e) {
         this.setData({
@@ -32,25 +33,38 @@ Page({
     onLoad() {
         //获取云端数据
         wx.cloud.init();
+
         const db = wx.cloud.database('start-project-8582df');
         const that = this;
-        db.collection("index_datas").doc('5cb16b578852e71926082a96').get({
-            success(msg) {
-                that.setData({
-                    datas: msg.data.datas,
-                    loading: true
-                })
-            },
-            fail: console.error
-        })
+        db.collection("index_datas")
+            .doc('5cb16b578852e71926082a96')
+            .get({
+                success(msg) {
+                    that.setData({
+                        datas: msg.data.datas,
+                        loading: true
+                    })
+                },
+                fail: console.error
+            });
+        db.collection('feedBack')
+            .get({
+                success(res) {
+                    console.log(res)
+                    that.setData({
+                        feedBackTxt: res
+                    })
+                }
+            })
     },
     onPullDownRefresh() {
         const that = this;
-        wx.startPullDownRefresh({
-            success(res) {
-                wx.cloud.init();
-                const db = wx.cloud.database('start-project-8582df');
-                db.collection("index_datas").doc('5cb16b578852e71926082a96').get({
+        wx.cloud.init();
+        const db = wx.cloud.database('start-project-8582df');
+        if (that.data.content) {
+            db.collection("index_datas")
+                .doc('5cb16b578852e71926082a96')
+                .get({
                     success(msg) {
                         that.setData({
                             datas: msg.data.datas
@@ -67,7 +81,27 @@ Page({
                         wx.stopPullDownRefresh()
                     }
                 })
-            }
-        })
+        }
+        if (that.data.help){
+            db.collection('feedBack')
+                .where({
+                    id: "1"
+                }).get({
+                    success(res) {
+                        that.setData({
+                            feedBackTxt: res
+                        })
+                        wx.stopPullDownRefresh();
+                        wx.showToast({
+                            title: '刷新成功',
+                            icon: 'success',
+                            duration: 1000
+                        })
+                    },
+                    fail(){
+                        wx.stopPullDownRefresh();
+                    }
+                })
+        }
     }
 })
